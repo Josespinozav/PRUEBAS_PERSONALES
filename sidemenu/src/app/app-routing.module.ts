142ad4ab7ca8a4f3cf1b0ad58112e51cd90f0b01
unknown <jos.espinozav@duocuc.cl>
2022-09-05 23:14:06 -0300

    1.6

--- sidemenu/src/app/app-routing.module.ts
+++ sidemenu/src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'folder/Inbox',
+    pathMatch: 'full'
+  },
+  {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
   },
